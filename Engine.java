ef6d35f28ec827386ef8c79fc43a28934db8b54d
Bharath Kaimal <bharath.kaimal@gmail.com>
2020-02-21 12:49:20 -0600

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -59,6 +59,12 @@ public class Engine {
   // Return true if the board is full
   // Return false if not
   public boolean checkTie() {
+	  for(int i=0;i<3;i++) {
+		  for(int j=0;j<3;j++) {
+			  if(board[i][j] == 0)
+				  return false;
+		  }
+	  }
     return true;
   }
 
